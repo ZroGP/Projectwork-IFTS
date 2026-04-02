a14f176d9e925dccb97fc8f6bdc64b6b6ba06cea
XDragonXn <elmotarajjimohammed@gmail.com>
2026-04-02 15:21:17 +0200

    Fixed buttons UI

--- projectwork-ifts/scripts/ui_menu.gd
+++ projectwork-ifts/scripts/ui_menu.gd
@@ -17,6 +17,12 @@ extends Control
 @onready var quit_button = %Quit  
 @onready var quit_popup = %Pop_Quit 
 @onready var no_quit = %No 
+@onready var yes_quit = %Yes
+
+# Languages
+@onready var account_button = %Button_Account  
+@onready var account_popup = %Pop_Account
+@onready var exit_account = %Exit_Account
 
 
 func _ready():
@@ -38,10 +44,14 @@ func _ready():
 	#Quit
 	quit_button.pressed.connect(_on_quit_button_pressed)
 	no_quit.pressed.connect(_on_no_quit_pressed)
-	
+	yes_quit.pressed.connect(_on_yes_quit_pressed)
 	quit_popup.hide()
 	
+	#Account
+	account_button.pressed.connect(_on_account_button_pressed)
+	exit_account.pressed.connect(_on_exit_account_pressed)
 	
+	account_popup.hide()
 func _on_settings_button_pressed():
 	menu_container.show()
 	
@@ -71,3 +81,15 @@ func _on_quit_button_pressed():
 func _on_no_quit_pressed():
 	quit_popup.hide()
 	menu_container.show()
+	
+func _on_account_button_pressed():
+	menu_container.hide()
+	account_popup.show()
+	
+func _on_yes_quit_pressed():
+	get_tree().quit()
+	
+func _on_exit_account_pressed():
+	account_popup.hide()
+	menu_container.show()
+	
